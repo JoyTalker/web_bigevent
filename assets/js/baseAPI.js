64aae829249efe86b56a750e2fb5b07adee8c643
it name <16656061912@163.com>
2024-03-17 11:10:47 +0800

    完成个人中心的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -8,16 +8,16 @@ $.ajaxPrefilter(function(options){
     // options.url ='根路径'+options.url
     options.url ='https://big-event-vue-api-t.itheima.net/'+options.url
     //统一为有权限的接口，设置headers请求头
-    if(options.url.indexof('/my/')!==-1){
+    if(options.url.indexOf('/my/')!==-1){
         options.headers={
         Authorization:localStorage.getItem('token')||''
     }
     //全局统一挂载 complete 回调函数
-         option.complete=function(res){
+         options.complete=function(res){
             console.log('执行了complete回调函数')
             console.log(res)
             //在complete回调函数中，可以使用res.responseJSON 拿到服务器响应回来的数据
-            if(res.responseISON.status===1&&res.responseISON.message==='身份认证失败！'){
+            if(res.responseSON.code===1&&res.responseJSON.message==='身份认证失败！'){
                 //强制清空 token
                 localStorage.removeItem('token')
                 //强制跳转到登录页面
